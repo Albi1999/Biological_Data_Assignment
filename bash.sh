f6ec0a0eb42a55e3b84c878b0a1ae633af15f672
Alberto Calabrese <albertocalabrese99@gmail.com>
2025-06-02 16:09:32 +0200

    Add files via upload

---
+++ bash.sh
@@ -0,0 +1,5 @@
+ncbi-blast-2.16.0+/bin/psiblast -subject
+Assignment/clustalo-I20241111-163722-0863-15646642-p1m.fasta -in_msa
+Assignment/clustalo-I20241111-163722-0863-15646642-p1m.fasta -out_ascii_pssm
+Assignment/clustalo-I20241111-163722-0863-15646642-p1m.pssm_ascii -out_pssm
+Assignment/clustalo-I20241111-163722-0863-15646642-p1m.pssm
